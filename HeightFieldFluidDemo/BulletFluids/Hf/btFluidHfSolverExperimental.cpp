0fcd98164a8ed51b08632c3f245e2a8de759241d
Jackson Lee <rtrius@gmail.com>
2012-11-29 18:03:16 -0800

    Test MacCormack advection, add new height update, fix boundary. (Also disable fluid advection;  substantially improves volume conservation.)
    Add: new height update.
    (Improves volume conservation for Demo 'Init_Ground')
    
    Fix: set velocity to 0 at edge boundaries.

--- HeightFieldFluidDemo/BulletFluids/Hf/btFluidHfSolverExperimental.cpp
+++ HeightFieldFluidDemo/BulletFluids/Hf/btFluidHfSolverExperimental.cpp
@@ -2,8 +2,243 @@
 
 #include "LinearMath/btMinMax.h"
 
+btScalar bilinearInterpolateWithBounds(const btFluidColumns& columns, const btAlignedObjectArray<btScalar>& array, 
+											btScalar iPos, btScalar jPos, btScalar& out_min, btScalar& out_max)
+{
+	int i = (int)iPos;
+	int j = (int)jPos;
 
-void btFluidHfSolverExperimental::advectVelocityX_New(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
+	btScalar iParam1 = iPos - btScalar(i);
+	btScalar iParam0 = btScalar(1.0) - iParam1;
+
+	btScalar jParam1 = jPos - btScalar(j);
+	btScalar jParam0 = btScalar(1.0) - jParam1;
+
+	btScalar SW = array[columns.getIndex(i, j)];
+	btScalar SE = array[columns.getIndex(i+1, j)];
+	btScalar NW = array[columns.getIndex(i, j+1)];
+	btScalar NE = array[columns.getIndex(i+1, j+1)];
+
+	out_min = btMin( btMin(SW, NW), btMin(SE, NE) );
+	out_max = btMax( btMax(SW, NW), btMax(SE, NE) );
+	
+	btScalar a = jParam0 * SW + jParam1 * NW;
+	btScalar b = jParam0 * SE + jParam1 * NE;
+	return iParam0 * a + iParam1 * b;
+}
+
+btScalar advectForwardWithBounds(const btFluidColumns& columns, const btAlignedObjectArray<btScalar>& array, 
+						btScalar i, btScalar j, btScalar vel_x, btScalar vel_z, btScalar dt, btScalar& out_min, btScalar& out_max)
+{
+	// trace particle backwards in time
+	btScalar srcI = i - vel_x * dt * columns.m_gridCellWidthInv;
+	btScalar srcJ = j - vel_z * dt * columns.m_gridCellWidthInv;
+
+	// srcI and srcJ are indices into the array,
+	// we need to clamp them to within the domain
+	srcI = btMax (srcI, btScalar(0.0));
+	srcI = btMin (srcI, btScalar(columns.m_numNodesX-1));
+	srcJ = btMax (srcJ, btScalar(0.0));
+	srcJ = btMin (srcJ, btScalar(columns.m_numNodesZ-1));
+
+	return bilinearInterpolateWithBounds(columns, array, srcI, srcJ, out_min, out_max);
+}
+btScalar btFluidHfSolverExperimental::advectReverse(const btFluidColumns& columns, const btAlignedObjectArray<btScalar>& array, 
+													btScalar i, btScalar j, btScalar vel_x, btScalar vel_z, btScalar dt)
+{
+	// trace particle forward in time
+	btScalar srcI = i + vel_x * dt * columns.m_gridCellWidthInv;
+	btScalar srcJ = j + vel_z * dt * columns.m_gridCellWidthInv;
+
+	// srcI and srcJ are indices into the array,
+	// we need to clamp them to within the domain
+	srcI = btMax (srcI, btScalar(0.0));
+	srcI = btMin (srcI, btScalar(columns.m_numNodesX-1));
+	srcJ = btMax (srcJ, btScalar(0.0));
+	srcJ = btMin (srcJ, btScalar(columns.m_numNodesZ-1));
+
+	return bilinearInterpolate(columns, array, srcI, srcJ);
+}
+
+void btFluidHfSolverExperimental::advectFluid_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
+{
+	//Forward advect
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += (columns.m_vel_x[index]+columns.m_vel_x[index+1]) * btScalar(0.5);
+			vel_z += (columns.m_vel_z[index]+columns.m_vel_z[index+columns.m_numNodesX]) * btScalar(0.5);
+
+			m_tempForwardAdvect[index] = advectForwardWithBounds(columns, columns.m_fluidDepth, btScalar(i), btScalar(j), vel_x, vel_z, dt,
+															m_tempForwardAdvectMin[index], m_tempForwardAdvectMax[index]);
+		}
+	}
+	
+	//Reverse advect to get error
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += (columns.m_vel_x[index]+columns.m_vel_x[index+1]) * btScalar(0.5);
+			vel_z += (columns.m_vel_z[index]+columns.m_vel_z[index+columns.m_numNodesX]) * btScalar(0.5);
+
+			m_tempReverseAdvect[index] = advectReverse(columns, m_tempForwardAdvect, btScalar(i), btScalar(j), vel_x, vel_z, dt);
+		}
+	}
+	
+	//Combine forward advect and reverse advect to get result
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+			
+			//Error is calculated by comparing the original fluidDepth with the forward-reverse advected fluidDepth
+			//As the fluid is advected twice, the error is also doubled
+			btScalar error = (columns.m_fluidDepth[index] - m_tempReverseAdvect[index]) * btScalar(0.5);
+			btScalar advectedValue = m_tempForwardAdvect[index] + error;
+			
+			//Revert to semi-Lagrangian if result is not within bounds of values used for bilinear interpolation
+			btScalar min = m_tempForwardAdvectMin[index];
+			btScalar max = m_tempForwardAdvectMax[index];
+			columns.m_fluidDepth[index] = (min <= advectedValue && advectedValue <= max) ? advectedValue : m_tempForwardAdvect[index];
+		}
+	}
+}
+void btFluidHfSolverExperimental::advectVelocityX_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
+{
+	//Forward advect
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += columns.m_vel_x[index];
+			vel_z += (columns.m_vel_z[index]+columns.m_vel_z[index-1]+columns.m_vel_z[index+columns.m_numNodesX]+columns.m_vel_z[index+columns.m_numNodesX-1]) * btScalar(0.25);
+
+			m_tempForwardAdvect[index] = advectForwardWithBounds(columns, columns.m_vel_x, btScalar(i), btScalar(j), vel_x, vel_z, dt,
+															m_tempForwardAdvectMin[index], m_tempForwardAdvectMax[index]);
+		}
+	}
+	
+	//Reverse advect to get error
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += columns.m_vel_x[index];
+			vel_z += (columns.m_vel_z[index]+columns.m_vel_z[index-1]+columns.m_vel_z[index+columns.m_numNodesX]+columns.m_vel_z[index+columns.m_numNodesX-1]) * btScalar(0.25);
+
+			m_tempReverseAdvect[index] = advectReverse(columns, m_tempForwardAdvect, btScalar(i), btScalar(j), vel_x, vel_z, dt);
+		}
+	}
+	
+	//Combine forward advect and reverse advect to get result
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+			
+			//As the fluid is advected twice, the error is also doubled
+			btScalar error = (columns.m_vel_x[index] - m_tempReverseAdvect[index]) * btScalar(0.5);
+			btScalar advectedValue = m_tempForwardAdvect[index] + error;
+			
+			//Revert to semi-Lagrangian if result is not within bounds of values used for bilinear interpolation
+			btScalar min = m_tempForwardAdvectMin[index];
+			btScalar max = m_tempForwardAdvectMax[index];
+			columns.m_vel_x[index] = (min <= advectedValue && advectedValue <= max) ? advectedValue : m_tempForwardAdvect[index];
+		}
+	}
+}
+void btFluidHfSolverExperimental::advectVelocityZ_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
+{
+
+	//Forward advect
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += (columns.m_vel_x[index]+columns.m_vel_x[index+1]+columns.m_vel_x[index-columns.m_numNodesX]+columns.m_vel_x[index-columns.m_numNodesX+1]) * btScalar(0.25);
+			vel_z += columns.m_vel_z[index];
+
+			m_tempForwardAdvect[index] = advectForwardWithBounds(columns, columns.m_vel_z, btScalar(i), btScalar(j), vel_x, vel_z, dt,
+															m_tempForwardAdvectMin[index], m_tempForwardAdvectMax[index]);
+		}
+	}
+	
+	//Reverse advect to get error
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+
+			btScalar vel_x = hfParameters.m_globalVelocityX;
+			btScalar vel_z = hfParameters.m_globalVelocityZ;
+
+			vel_x += (columns.m_vel_x[index]+columns.m_vel_x[index+1]+columns.m_vel_x[index-columns.m_numNodesX]+columns.m_vel_x[index-columns.m_numNodesX+1]) * btScalar(0.25);
+			vel_z += columns.m_vel_z[index];
+
+			m_tempReverseAdvect[index] = advectReverse(columns, m_tempForwardAdvect, btScalar(i), btScalar(j), vel_x, vel_z, dt);
+		}
+	}
+	
+	//Combine forward advect and reverse advect to get result
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index]) continue;
+			
+			//As the fluid is advected twice, the error is also doubled
+			btScalar error = (columns.m_vel_z[index] - m_tempReverseAdvect[index]) * btScalar(0.5);
+			btScalar advectedValue = m_tempForwardAdvect[index] + error;
+			
+			//Revert to semi-Lagrangian if result is not within bounds of values used for bilinear interpolation
+			btScalar min = m_tempForwardAdvectMin[index];
+			btScalar max = m_tempForwardAdvectMax[index];
+			columns.m_vel_z[index] = (min <= advectedValue && advectedValue <= max) ? advectedValue : m_tempForwardAdvect[index];
+		}
+	}
+}
+
+
+void btFluidHfSolverExperimental::advectVelocityX_new(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
 {
 	for (int j = 1; j < columns.m_numNodesZ-1; j++)
 	{
@@ -31,7 +266,7 @@ void btFluidHfSolverExperimental::advectVelocityX_New(const btScalar dt, const b
 		}
 	}
 }
-void btFluidHfSolverExperimental::advectVelocityZ_New(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
+void btFluidHfSolverExperimental::advectVelocityZ_new(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
 {
 	for (int j = 1; j < columns.m_numNodesZ-1; j++)
 	{
@@ -45,7 +280,6 @@ void btFluidHfSolverExperimental::advectVelocityZ_New(const btScalar dt, const b
 
 			vel_x += (columns.m_vel_x[index]+columns.m_vel_x[index+1]+columns.m_vel_x[index-columns.m_numNodesX]+columns.m_vel_x[index-columns.m_numNodesX+1]) * btScalar(0.25);
 			vel_z += columns.m_vel_z[index];
-			
 
 			columns.m_temp[index] = advect(columns, columns.m_vel_z, btScalar(i), btScalar(j), vel_x, vel_z, dt);
 		}
@@ -61,10 +295,50 @@ void btFluidHfSolverExperimental::advectVelocityZ_New(const btScalar dt, const b
 	}
 }
 
+void btFluidHfSolverExperimental::updateHeight_new(const btScalar dt, btFluidColumns& columns)
+{
+	for (int j = 1; j < columns.m_numNodesZ-1; j++)
+	{
+		for (int i = 1; i < columns.m_numNodesX-1; i++)
+		{
+			int index = columns.getIndex(i, j);
+			if (!columns.m_active[index])
+			{
+				columns.m_combinedHeight[index] = columns.m_ground[index] + columns.m_fluidDepth[index];
+				continue;
+			}
+			
+			btScalar u1, u2, v1, v2;
+			if(1)
+			{
+				u1 = columns.m_vel_x[index+1] * (columns.m_fluidDepth[index] + columns.m_fluidDepth[index+1]) * btScalar(0.5);
+				u2 = columns.m_vel_x[index] * (columns.m_fluidDepth[index] + columns.m_fluidDepth[index-1]) * btScalar(0.5);
+				v1 = columns.m_vel_z[index+columns.m_numNodesX] * (columns.m_fluidDepth[index] + columns.m_fluidDepth[index+columns.m_numNodesX]) * btScalar(0.5);
+				v2 = columns.m_vel_z[index] * (columns.m_fluidDepth[index] + columns.m_fluidDepth[index-columns.m_numNodesX]) * btScalar(0.5);
+			}
+			else
+			{
+				//Evaluate height in upwind direction(unstable)
+				u1 = columns.m_vel_x[index+1] * ( columns.m_vel_x[index+1] < btScalar(0.0) ) ? columns.m_fluidDepth[index+1] : columns.m_fluidDepth[index];
+				u2 = columns.m_vel_x[index] * ( columns.m_vel_x[index] < btScalar(0.0) ) ? columns.m_fluidDepth[index] : columns.m_fluidDepth[index-1];
+				v1 = columns.m_vel_z[index+columns.m_numNodesX] * ( columns.m_vel_z[index+columns.m_numNodesX] < btScalar(0.0) ) ? columns.m_fluidDepth[index+columns.m_numNodesX] : columns.m_fluidDepth[index];
+				v2 = columns.m_vel_z[index] * ( columns.m_vel_z[index] < btScalar(0.0) ) ? columns.m_fluidDepth[index] : columns.m_fluidDepth[index-columns.m_numNodesX];
+			}
+			
+			btScalar deta = ((u1 - u2) + (v1 - v2)) * (-columns.m_gridCellWidthInv);
+			columns.m_fluidDepth[index] += deta * dt;
+			columns.m_fluidDepth[index] = btMax( columns.m_fluidDepth[index], btScalar(0.0) );
+			
+			columns.m_combinedHeight[index] = columns.m_ground[index] + columns.m_fluidDepth[index];
+		}
+	}
+}
+
 void btFluidHfSolverExperimental::applyBoundaryConditions(btFluidColumns& columns)
 {
 	const btScalar ACTIVE_CELL_EPSILON = btScalar(0.0001) * columns.m_gridCellWidth;
-
+	
+	//
 	for (int j = 1; j < columns.m_numNodesZ-1; j++)
 		for (int i = 1; i < columns.m_numNodesX-1; i++)
 		{
@@ -90,24 +364,32 @@ void btFluidHfSolverExperimental::applyBoundaryConditions(btFluidColumns& column
 		int indexL = columns.getIndex(0, j);						//Left(X-)
 		columns.m_fluidDepth[indexL] = BOUNDARY_FLUID_DEPTH;
 		columns.m_combinedHeight[indexL] = columns.m_ground[indexL];
+		
+		columns.m_vel_x[indexL+1] = btScalar(0.0);
 	}
-	for(int j = 0; j < columns.m_numNodesZ; j++)					//Right(X+)
+	for(int j = 0; j < columns.m_numNodesZ; j++)
 	{
-		int indexR = columns.getIndex(columns.m_numNodesX-1, j);
+		int indexR = columns.getIndex(columns.m_numNodesX-1, j);	//Right(X+)
 		columns.m_fluidDepth[indexR] = BOUNDARY_FLUID_DEPTH;
 		columns.m_combinedHeight[indexR] = columns.m_ground[indexR];
+		
+		columns.m_vel_x[indexR] = btScalar(0.0);
 	}
 	for(int i = 0; i < columns.m_numNodesX; i++)
 	{
-		int indexT = columns.getIndex(i, 0);						//Top(Z-)
-		columns.m_fluidDepth[indexT] = BOUNDARY_FLUID_DEPTH;
-		columns.m_combinedHeight[indexT] = columns.m_ground[indexT];
-	}
-	for(int i = 0; i < columns.m_numNodesX; i++)
-	{
-		int indexB = columns.getIndex(i, columns.m_numNodesZ-1);	//Bottom(Z+)
+		int indexB = columns.getIndex(i, 0);						//Bottom(Z-)
 		columns.m_fluidDepth[indexB] = BOUNDARY_FLUID_DEPTH;
 		columns.m_combinedHeight[indexB] = columns.m_ground[indexB];
+		
+		columns.m_vel_z[indexB+columns.m_numNodesX] = btScalar(0.0);
+	}
+	for(int i = 0; i < columns.m_numNodesX; i++)
+	{
+		int indexT = columns.getIndex(i, columns.m_numNodesZ-1);	//Top(Z+)
+		columns.m_fluidDepth[indexT] = BOUNDARY_FLUID_DEPTH;
+		columns.m_combinedHeight[indexT] = columns.m_ground[indexT];
+		
+		columns.m_vel_z[indexT] = btScalar(0.0);
 	}
 }
 
--- HeightFieldFluidDemo/BulletFluids/Hf/btFluidHfSolverExperimental.h
+++ HeightFieldFluidDemo/BulletFluids/Hf/btFluidHfSolverExperimental.h
@@ -6,23 +6,54 @@
 
 class btFluidHfSolverExperimental : public btFluidHfSolverDefault
 {
+	//For MacCormack advection
+	btAlignedObjectArray<btScalar> m_tempForwardAdvect;
+	btAlignedObjectArray<btScalar> m_tempForwardAdvectMin;
+	btAlignedObjectArray<btScalar> m_tempForwardAdvectMax;
+	btAlignedObjectArray<btScalar> m_tempReverseAdvect;
+	
 public:
 	virtual void stepSimulation(btScalar timeStep, const btFluidHfParameters& hfParameters, btFluidColumns& columns)
 	{
-		btScalar initialHeight = getTotalFluidHeight(columns);
+		int numColumns = columns.numColumns();
+		if( m_tempForwardAdvect.size() < numColumns )
+		{
+			m_tempForwardAdvect.resize(numColumns);
+			m_tempForwardAdvectMin.resize(numColumns);
+			m_tempForwardAdvectMax.resize(numColumns);
+			m_tempReverseAdvect.resize(numColumns);
+		}
+		
 	
 		transferDisplaced(hfParameters.m_volumeDisplacementScale, columns);
 		
-		advectEta(timeStep, hfParameters, columns);
+		btScalar initialHeight = getTotalFluidHeight(columns);
+		
+		const bool USE_MACCORMACK_METHOD = false;
+		if(USE_MACCORMACK_METHOD)
+		{
+			//advectFluid_macCormack(timeStep, hfParameters, columns);
+			advectVelocityX_macCormack(timeStep, hfParameters, columns);
+			advectVelocityZ_macCormack(timeStep, hfParameters, columns);
+		}
+		else
+		{
+			//advectEta(timeStep, hfParameters, columns);
+			advectVelocityX_new(timeStep, hfParameters, columns);
+			advectVelocityZ_new(timeStep, hfParameters, columns);
+		}
+		
+		//Most volume loss occurs during advection
 		{
 			btScalar heightAfterAdvection = getTotalFluidHeight(columns);
 			printf("heightAfterAdvection - initialHeight: %f \n", heightAfterAdvection - initialHeight);
+			printf("initialHeight: %f \n", initialHeight);
+			printf("heightAfterAdvection: %f \n", heightAfterAdvection);
 		}
 		
-		advectVelocityX_New(timeStep, hfParameters, columns);
-		advectVelocityZ_New(timeStep, hfParameters, columns);
-		
-		updateHeight(timeStep, columns);
+		//applyBoundaryConditions(columns);
+		//updateHeight(timeStep, columns);
+		updateHeight_new(timeStep, columns);
 		
 		if(1)
 		{
@@ -42,13 +73,24 @@ public:
 		
 		btScalar finalHeight = getTotalFluidHeight(columns);
 		printf("finalHeight - initialHeight: %f \n", finalHeight - initialHeight);
+		printf("initialHeight: %f \n", initialHeight);
 		printf("finalHeight: %f \n", finalHeight);
 		printf("\n");
 	}
 	
 protected:
-	static void advectVelocityX_New(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
-	static void advectVelocityZ_New(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+
+	static btScalar advectReverse(const btFluidColumns& columns, const btAlignedObjectArray<btScalar>& array, 
+									btScalar i, btScalar j, btScalar vel_x, btScalar vel_z, btScalar dt);
+
+	void advectFluid_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+	void advectVelocityX_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+	void advectVelocityZ_macCormack(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+	
+	static void advectVelocityX_new(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+	static void advectVelocityZ_new(const btScalar dt, const btFluidHfParameters& hfParameters, btFluidColumns& columns);
+	
+	static void updateHeight_new(const btScalar dt, btFluidColumns& columns);
 	
 	static void applyBoundaryConditions(btFluidColumns& columns);
 };
